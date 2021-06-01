bdf6d9b78007044f355ce106162ad2fc1bca8ef1
ayaka.fyjita <ayaka@allexceed.co.jp>
2021-06-01 15:53:03 +0900

    index (TopPage)

---
+++ WebContent/WEB-INF/views/tasks/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<c:import url="../layout/app.jsp">
+    <c:param name="content">
+        <h2>タスク一覧</h2>
+        <ul>
+            <c:forEach var="task" items="${tasks}">
+                <li>
+                    <a href="${pageContext.request.contextPath}/show?id=${task.id}">
+                        <c:out value="${task.id}" />
+                    </a>
+                    ： <c:out value="${task.task}" />
+                </li>
+            </c:forEach>
+        </ul>
+        <br />
+        <p><a href="${pageContent.request.contextPath}/new">新しいタスクを作成</a></p>
+    </c:param>
+</c:import>
--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -50,18 +51,16 @@ public class IndexServlet extends HttpServlet {
         //long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
         //                            .getSingleResult();
 
-        //em.close();
+        em.close();
 
-        //request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks", tasks);
         //request.setAttribute("tasks_count", tasks_count);
         //request.setAttribute("page", page);
 
-        //RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/message/index.jsp");
-        //rd.forward(request, response);
+        // index.jsp の呼び出し
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
 
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
-
-        em.close();
+        //response.getWriter().append(Integer.valueOf(tasks.size()).toString());
     }
-
 }
